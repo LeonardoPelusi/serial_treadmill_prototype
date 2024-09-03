cf377565bec4c88d129705aca78a93f1242484b6
thiagoevosystems <thiago.tadeu@trendxbrasil.com>
2024-09-03 19:20:55 -0300

    melhorias nos testes main_three

--- lib/main_three.dart
+++ lib/main_three.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:serial_to_usb_treadmill_prototype/treadmill_protocol.dart';
 import 'package:usb_serial_for_android/transaction.dart';
 import 'package:usb_serial_for_android/usb_device.dart';
 import 'package:usb_serial_for_android/usb_event.dart';
 import 'package:usb_serial_for_android/usb_port.dart';
 import 'package:usb_serial_for_android/usb_serial_for_android.dart';
-import 'package:collection/collection.dart';
+import 'enums.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -31,17 +33,14 @@ class _MyAppState extends State<MyApp> {
 
   Future<bool> _connectTo(UsbDevice? device) async {
     _serialData.clear();
-
     if (_transaction != null) {
       _transaction!.dispose();
       _transaction = null;
     }
-
     if (_port != null) {
       _port!.close();
       _port = null;
     }
-
     if (device == null) {
       _device = null;
       setState(() {
@@ -84,6 +83,9 @@ class _MyAppState extends State<MyApp> {
 
     var response = await _transaction?.transaction(_port!, Uint8List.fromList([65,66,13,10]), Duration(seconds: 1) );
     print("aqui a resposta: $response");
+    setState(() {
+      _serialData.add(Text('${response.toString()}\n'));
+    });
 
   }
 
@@ -151,105 +153,26 @@ class _MyAppState extends State<MyApp> {
                         scrollDirection: Axis.horizontal,
                         child: Row(
                           children: [
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Initiate Treadmill'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0xa0, 0x80, 0x10, 0x78, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
-                            const SizedBox(width: 20),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Verify Error'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0x1a, 0x8b, 0x3e, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Choose Speed Command'),
-                                onPressed: () async {
-                                  commandType = WriteCommandType.speed;
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Choose Inclination Command'),
-                                onPressed: () async {
-                                  commandType = WriteCommandType.inclination;
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Read Speed'),
-                                onPressed: () async {
-                                  final List<int> readSpeed = [0xf6, 0x10, 0x8c, 0xbe, 0xf4];
-                                  await _sendCommand(readSpeed);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Illegal command'),
-                                onPressed: () async {
-                                  final List<int> readSpeed = [0xf6, 0x00, 0x00, 0x00, 0xf4];
-                                  await _sendCommand(readSpeed);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Stop command 0xff'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0xa0, 0xff, 0xf7, 0x00, 0x39, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Stop command 0xc0'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0xa0, 0xc0, 0xe0, 0x79, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('Stop command 0x00'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0xa0, 0x00, 0xb0, 0x79, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 5),
-                              child: ElevatedButton(
-                                child: const Text('zerado'),
-                                onPressed: () async {
-                                  final List<int> startTreadmill = [0xf6, 0x90, 0x00, 0x01, 0x2d, 0xb0, 0xf4];
-                                  await _sendCommand(startTreadmill);
-                                },
-                              ),
-                            ),
+                            _chooseCommandType('Choose Speed Command',
+                                WriteCommandType.speed),
+                            _chooseCommandType('Choose Inclination Command',
+                                WriteCommandType.inclination),
+                            _commandButton('Initiate Treadmill',
+                                [0xf6, 0xa0, 0x80, 0x10, 0x78, 0xf4]),
+                            _commandButton('Verify Error',
+                                [0xf6, 0x1a, 0x8b, 0x3e, 0xf4]),
+                            _commandButton('Read Speed',
+                                [0xf6, 0x10, 0x8c, 0xbe, 0xf4]),
+                            _commandButton('Illegal command',
+                                [0xf6, 0x00, 0x00, 0x00, 0xf4]),
+                            _commandButton('Stop command 0xff',
+                                [0xf6, 0xa0, 0xff, 0xf7, 0x00, 0x39, 0xf4]),
+                            _commandButton('Stop command 0xc0',
+                                [0xf6, 0xa0, 0xc0, 0xe0, 0x79, 0xf4]),
+                            _commandButton('Stop command 0x00',
+                                [0xf6, 0xa0, 0x00, 0xb0, 0x79, 0xf4]),
+                            _commandButton('zerado',
+                                [0xf6, 0x90, 0x00, 0x01, 0x2d, 0xb0, 0xf4]),
                           ],
                         ),
                       ),
@@ -272,7 +195,7 @@ class _MyAppState extends State<MyApp> {
                               }
                               int data = int.parse(_textController.text);
                               List<int>? dataToSend =
-                              writeDataToInverter(value: data, type: commandType);
+                              TreadmillProtocol.writeDataToInverter(value: data, type: commandType);
                               await _sendCommand(dataToSend);
                             },
                             child: const Text("Send"),
@@ -286,167 +209,26 @@ class _MyAppState extends State<MyApp> {
               )),
         ));
   }
-}
 
-
-List<int> writeDataToInverter(
-    {required num value, required WriteCommandType type}) {
-  List<int> reqAndIns = [0xf6, type.toINS()];
-  List<int> dataRaw =
-  _getDataFormatted(value: value, multFactor: type.toMultiFactor());
-  List<int> dataSplit = _splitCode(dataRaw);
-  List<int> crcRaw = _getCrc(type.toINS(), dataRaw);
-  List<int> crcSplit = _splitCode(crcRaw);
-  List<int> end = [0xf4];
-
-  List<int> finalData = reqAndIns + dataSplit + crcSplit + end;
-  return finalData;
-}
-
-enum WriteCommandType {
-  speed,
-  inclination
-}
-
-extension WriteCommandTypeToINS on WriteCommandType {
-  int toINS() {
-    switch(this) {
-      case WriteCommandType.speed:
-        return 0x90;
-      case WriteCommandType.inclination:
-        return 0x98;
-    }
-  }
-}
-
-extension CommandTypeToMultiFactor on WriteCommandType {
-  num toMultiFactor() {
-    switch(this) {
-      case WriteCommandType.speed:
-        return 1;
-      case WriteCommandType.inclination:
-        return 66.6;
-    }
-  }
-}
-
-List<int> readCommandToInverter({required ReadCommandType type}) {
-  List<int> reqAndIns = [0xf6, type.toINS()];
-  List<int> crc = _getCrc(type.toINS(), []);
-  List<int> end = [0xf4];
-
-  List<int> finalData = reqAndIns + crc + end;
-  return finalData;
-}
-
-List<int> _getDataFormatted({required num value, required num multFactor}) {
-  int convertedValue = (value * multFactor).round();
-
-  int partOne = convertedValue >> 8 & 0xFF;
-  int partTwo = convertedValue & 0xFF;
-
-  return [partOne, partTwo];
-}
-
-List<int> _splitCode(List<int> value) {
-  final List<int> command = [];
-
-  for (int hexNum in value) {
-    if (hexNum >= 0xf0 && hexNum <= 0xff) {
-      int part1 = 0xf7;
-      int part2 = hexNum - 0xf0;
-      command.add(part1);
-      command.add(part2);
-    } else {
-      command.add(hexNum);
-    }
+  Widget _commandButton(String title, List<int> command) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: ElevatedButton(
+        child: Text(title),
+        onPressed: () async {
+          await _sendCommand(command);
+        },
+      ),
+    );
   }
 
-  return command;
-}
-
-List<int> _getCrc(int ins, List<int>? data) {
-  List<int> command = [ins] + data!;
-
-  int length = command.length;
-
-  int regCRC = 0xffff;
-  int index = 0;
-
-  while (length > 0) {
-    regCRC ^= command[index]++;
-
-    for (int i = 0; i < 8; i++) {
-      if (regCRC & 0x01 == 1) {
-        regCRC = (regCRC >> 1) ^ 0xa001;
-      } else {
-        regCRC = regCRC >> 1;
-      }
-    }
-    length--;
-    index++;
-  }
-  return [regCRC >> 8 & 0xFF, regCRC & 0xFF];
-}
-
-int _readDataFromInverter(
-    {required Uint8List answer, required ReadCommandType type}) {
-  List<int> dataAndStu = [answer[2], answer[3], answer[4]];
-  List<int> crcCalculated = _getCrc(answer[1], dataAndStu);
-  List<int> crcfromInverter = [answer[5], answer[6]];
-
-  bool canContinue = _confirmCRC(crcCalculated, crcfromInverter);
-
-  if (canContinue) {
-    String dataFormat =
-        answer[2].toRadixString(16) + answer[3].toRadixString(16);
-    int finalValue = int.parse(dataFormat, radix: 16);
-    return (finalValue / type.toDivideFactor()).round();
-  } else {
-    debugPrint('bytes corrompidos');
-    return 0;
-  }
-}
-
-bool _confirmCRC(List<int> crcCalculated, List<int> crcFromInverter) {
-  Function eq = const ListEquality().equals;
-  if (eq(crcCalculated, crcFromInverter)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-enum ReadCommandType {
-  speed,
-  inclinationCMD,
-  inclinationPOS
-}
-
-extension ReadCommandTypeToINS on ReadCommandType {
-  int toINS() {
-    switch(this) {
-      case ReadCommandType.speed:
-        return 0x10;
-      case ReadCommandType.inclinationCMD:
-        return 0x18;
-      case ReadCommandType.inclinationPOS:
-        return 0x19;
-    }
-  }
-}
-
-// para velocidade, usamos a mesma relação velocidadeXfrequência expressa
-// no CommandTypeToMultiFactor on WriteCommandType
-extension CommandTypeToDivideFactor on ReadCommandType {
-  double toDivideFactor() {
-    switch (this) {
-      case ReadCommandType.speed:
-        return 1;
-      case ReadCommandType.inclinationCMD:
-        return 66.6;
-      case ReadCommandType.inclinationPOS:
-        return 66.6;
-    }
+  Widget _chooseCommandType(String title, WriteCommandType type) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: ElevatedButton(
+        child: Text(title),
+        onPressed: () async => commandType = type
+      ),
+    );
   }
 }
